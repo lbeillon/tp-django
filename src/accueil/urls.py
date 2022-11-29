0dc5840fa754c6e13eb95f9bb876eb28a2751467
lbeillon <116715325+lbeillon@users.noreply.github.com>
2022-11-29 23:12:48 +0100

    update html helloworld et sum
    afficher la date dans helloworld : done
    créer une vue qui fait la somme de nombre dans l'url et afficher la somme dans sum.html : done

--- src/accueil/urls.py
+++ src/accueil/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import helloworld
+from .views import helloworld, somme
 
 urlpatterns = [
     path('', helloworld, name='accueil'),
+    path('<num1>/<num2>/', somme, name='somme'),
 ]
