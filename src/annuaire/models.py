0209c66fc2f706ed53635d16dfe30124b60e0b45
lbeillon <116715325+lbeillon@users.noreply.github.com>
2022-12-09 22:58:35 +0100

    update tp model

--- src/annuaire/models.py
+++ src/annuaire/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    telephone = models.CharField(max_length=10)
+
+
+
+
